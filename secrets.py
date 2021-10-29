d4860793f17cb16e75563ee395e02e38f54cf386
Samantha Medeiros <samanthadmedeiros@outlook.com>
2021-10-29 12:39:52 -0300

    create secrets.py

---
+++ secrets.py
@@ -0,0 +1,7 @@
+# Basta colocar seu nome de usuário aqui, por exemplo: 22gvuymg64y3e3zciarqaypoy
+user_id = "seu_username_aqi"
+
+# Essas chaves podem ser obtidas por meio da API do Spotify
+client_id="client_id"
+client_secret="client_secret"
+redirect_uri="redirect_uri"
